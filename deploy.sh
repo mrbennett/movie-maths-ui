a1be6470d04e42682a3badb2775bbb84021da33e
Matt Bennett <matt.bennett@xero.com>
2018-04-06 14:01:20 +1200

    new ec2 + deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-set REACT_APP_TMDB_KEY="cfe5cb91d24d4b7fb494300e0293c557"
-
-npm run build
+REACT_APP_TMDB_KEY="yourKey" npm run build
 
 aws s3 sync build/ s3://hannibal-vector-front-end --acl public-read
